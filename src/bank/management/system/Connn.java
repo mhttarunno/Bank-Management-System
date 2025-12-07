e872263510c4b4198098dadf594ecdc4c877bc01
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2025-12-07 23:28:50 +0600

    Add files via upload

---
+++ src/bank/management/system/Connn.java
@@ -0,0 +1,26 @@
+package bank.management.system;
+
+import java.sql.*;
+
+public class Connn {
+    Connection connection;
+    Statement statement;
+
+    public Connn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/bankManage",
+                    "root",
+                    "Creategmail@47"
+            );
+
+            statement = connection.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
